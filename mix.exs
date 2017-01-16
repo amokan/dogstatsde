fc828b408dcbb73d7875f34f1644d849e8c1dd61
Adam Mokan <amokan@gmail.com>
2017-01-16 12:38:29 -0700

    mix file fix

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Dogstatsd do
        elixir: "~> 1.2",
        build_embedded: Mix.env == :prod,
        start_permanent: Mix.env == :prod,
-       deps: deps]
+       deps: deps()]
     end
 
     # Configuration for the OTP application
